2dd5795015512e5ff3ffb2c0a4d3a06722c06c5c
Dindu <dindusam@gmail.com>
2026-09-23 12:13:01 -0500

    Disable one-minute sniper entry gate

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ import os
 
 os.environ.setdefault("ALPACA_FEED", "iex")
 os.environ.setdefault("MOMENTUM_BREAKOUT_BYPASS_ENABLED", "1")
+os.environ["ONE_MINUTE_ENTRY_ENABLED"] = "0"
+os.environ["ENTRY_TIMING_V1_ENABLED"] = "0"
 
 from spy_options_poll_bot import main as run_options_bot
 
